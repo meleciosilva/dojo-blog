f5aec8ede5a8ad2f4be62a7301a6e5c3898e8bc4
meleciosilva <mpsilva94@gmail.com>
2021-04-07 16:02:01 -0400

    add blog state

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...1', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...2', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...3', author: 'mario', id: 3 }
+  ]);
+  
   return ( 
     <div className="home">
-      <h2>Home Page</h2>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
    );
 }
